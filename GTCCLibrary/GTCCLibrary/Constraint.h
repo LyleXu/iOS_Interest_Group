32e6f806e6f9d679585c4ea95ebd01180ca5f942
LyleXu <lylexu@192.168.3.100>
2013-01-28 20:34:30 +0800

    note book test server

--- GTCCLibrary/GTCCLibrary/Constraint.h
+++ GTCCLibrary/GTCCLibrary/Constraint.h
@@ -11,8 +11,8 @@
 
 #define USERNAME    @"USERNAME"
 //#define ServerHost @"http://192.168.1.101:8080/gtcclibrary/"
-#define ServerHost @"http://localhost/~lylexu/gtcclibrary/"
-//#define ServerHost @"http://129.223.252.209:8080/gtcclibrary/"
+//#define ServerHost @"http://localhost/~lylexu/gtcclibrary/"
+#define ServerHost @"http://129.223.252.221:8080/gtcclibrary/"
 
 #define ServiceAddress @"amfphp/index.php"
 #define ServerImagePath @"Images/"    
