dde919dfcc05157c52b368f4f8b7c92a34a7c53d
gtcc <chanxl0009@gmail.com>
2013-05-30 00:36:17 +0800

    sync

--- GTCCLibrary/GTCCLibrary/Constraint.h
+++ GTCCLibrary/GTCCLibrary/Constraint.h
@@ -10,9 +10,9 @@
 #define GTCCLibrary_Constraint_h
 
 #define USERNAME    @"USERNAME"
-//#define ServerHost @"http://192.168.1.101:8080/gtcclibrary/"
+#define ServerHost @"http://192.168.23.1:8080/gtcclibrary/"
 //#define ServerHost @"http://localhost/~lylexu/gtcclibrary/"
-#define ServerHost @"http://129.223.252.221:8080/gtcclibrary/"
+//#define ServerHost @"http://129.223.252.205:8080/gtcclibrary/"
 
 #define ServiceAddress @"amfphp/index.php"
 #define ServerImagePath @"Images/"    
